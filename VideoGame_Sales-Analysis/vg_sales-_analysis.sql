646465bc3201d6ca3615fd0325140ebcc8cc88e2
Carlos Capili <carloscapili21@gmail.com>
2024-06-30 15:10:03 -0400

    Added sql file

---
+++ VideoGame_Sales-Analysis/vg_sales-_analysis.sql
@@ -0,0 +1,24 @@
+-- What consoles/devices are used?
+-- SELECT
+-- 	console
+-- FROM vg_sales
+-- GROUP BY console
+-- ORDER BY console;
+
+-- Who are the various publishers?
+-- SELECT 
+-- 	publisher
+-- FROM vg_sales
+-- GROUP BY publisher
+-- ORDER BY publisher;
+
+-- Who are the various developers?
+-- SELECT 
+-- 	developer
+-- FROM vg_sales
+-- GROUP BY developer
+-- ORDER BY developer;
+
+-- 
+SELECT *
+FROM vg_sales
