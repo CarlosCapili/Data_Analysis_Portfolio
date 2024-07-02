81b924dadfb57b4975ba400018e7048a4d1672b4
Carlos Capili <carloscapili21@gmail.com>
2024-07-01 22:48:47 -0400

    Added queries about sales by region

--- VideoGame_Sales-Analysis/vg_sales-_analysis.sql
+++ VideoGame_Sales-Analysis/vg_sales-_analysis.sql
@@ -97,24 +97,24 @@
 -- ORDER BY ptotal_sales DESC;
 
 -- What are the top-selling games for each publisher
-WITH top_selling_game AS (	
-	SELECT
-		publisher,
-		title,
-		SUM(total_sales) AS total_game_sales,
-		ROW_NUMBER() OVER(PARTITION BY publisher ORDER BY SUM(total_sales) DESC) AS rn
-	FROM vg_sales
-	WHERE total_sales IS NOT NULL
-	GROUP BY publisher, title
-)
+-- WITH top_selling_game AS (	
+-- 	SELECT
+-- 		publisher,
+-- 		title,
+-- 		SUM(total_sales) AS total_game_sales,
+-- 		ROW_NUMBER() OVER(PARTITION BY publisher ORDER BY SUM(total_sales) DESC) AS rn
+-- 	FROM vg_sales
+-- 	WHERE total_sales IS NOT NULL
+-- 	GROUP BY publisher, title
+-- )
 
-SELECT 
-	publisher,
-	title,
-	total_game_sales
-FROM top_selling_game
-WHERE rn = 1
-ORDER BY total_game_sales DESC;
+-- SELECT 
+-- 	publisher,
+-- 	title,
+-- 	total_game_sales
+-- FROM top_selling_game
+-- WHERE rn = 1
+-- ORDER BY total_game_sales DESC;
 
 -- Show the total sales of the Call of Duty franchise
 -- SELECT
@@ -128,15 +128,80 @@ ORDER BY total_game_sales DESC;
 -- 	AND total_sales IS NOT NULL
 -- ORDER BY release_date, title, total_sales DESC;
 
-
-
--- Which year had the highest sales?
-
 -- What titles are popular in one region but flop in another
 
 -- Top Rockstar Games games based on sales
 
 -- What is Call of Duty has the highest sales from Activision and who is the developer?
+
+-- SALES BY REGION
+
+-- How are the sales distributed across different regions?
+-- SELECT 
+-- 	title,
+-- 	console,
+-- 	publisher,
+-- 	ROUND(na_sales/total_sales, 2) AS na_sales_ratio,
+-- 	ROUND(jp_sales/total_sales, 2) AS jp_sales_ratio,
+-- 	ROUND(pal_sales/total_sales, 2) AS euro_afr_sales_ratio,
+-- 	ROUND(other_sales/total_sales, 2) AS restofworld_sales_ratio
+-- FROM vg_sales
+-- WHERE total_sales IS NOT NULL
+-- 	AND na_sales IS NOT NULL
+-- 	AND jp_sales IS NOT NULL
+-- 	AND pal_sales IS NOT NULL
+-- 	AND other_sales IS NOT NULL
+-- ORDER BY publisher, title;
+
+-- For each region display the game with the highest sales for each year
+SELECT
+	title,
+	publisher,
+	EXTRACT(YEAR FROM release_date) AS release_year,
+	SUM(na_sales) AS total_na_sales,
+	SUM(jp_sales) AS total_jp_sales,
+	SUM(pal_sales) AS total_pal_sales,
+	SUM(other_sales) AS total_other_sales
+FROM vg_sales
+WHERE total_sales IS NOT NULL
+	AND na_sales IS NOT NULL
+	AND jp_sales IS NOT NULL
+	AND pal_sales IS NOT NULL
+	AND other_sales IS NOT NULL
+GROUP BY title, publisher, EXTRACT(YEAR FROM release_date)
+ORDER BY release_year 
+
+
+-- For each game display the region with the highest sales
+-- WITH sales_per_region AS (
+-- 	SELECT
+-- 		title,
+-- 		publisher,
+-- 		SUM(na_sales) AS total_na_sales,
+-- 		SUM(jp_sales) AS total_jp_sales,
+-- 		SUM(pal_sales) AS total_pal_sales,
+-- 		SUM(other_sales) AS total_other_sales
+-- 	FROM vg_sales
+-- 	WHERE total_sales IS NOT NULL
+-- 		AND na_sales IS NOT NULL
+-- 		AND jp_sales IS NOT NULL
+-- 		AND pal_sales IS NOT NULL
+-- 		AND other_sales IS NOT NULL
+-- 	GROUP BY title, publisher
+-- )
+
+-- SELECT 
+-- 	title,
+-- 	publisher,
+-- 	CASE 
+-- 		WHEN total_na_sales >= total_jp_sales AND total_na_sales >= total_pal_sales AND total_na_sales >= total_other_sales THEN 'North America'
+-- 		WHEN total_jp_sales >= total_na_sales AND total_jp_sales >= total_pal_sales AND total_jp_sales >= total_other_sales THEN 'Japan'
+-- 		WHEN total_pal_sales >= total_na_sales AND total_pal_sales >= total_jp_sales AND total_pal_sales >= total_other_sales THEN 'Europe and Africa'
+-- 		WHEN total_other_sales >= total_na_sales AND total_other_sales >= total_jp_sales AND total_other_sales >= total_pal_sales THEN 'Rest of World'
+-- 		ELSE 'Cannot be determined'
+-- 	END AS highest_sale_region
+-- FROM sales_per_region
+
 
 
 -- SELECT *
