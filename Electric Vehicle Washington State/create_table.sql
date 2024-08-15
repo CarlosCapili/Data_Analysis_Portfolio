b4653ebc043c82aceb526302e5f59943bcd13f14
Carlos_Cap21 <59804756+CarlosCapili@users.noreply.github.com>
2024-08-14 22:49:27 -0400

    Add files via upload

---
+++ Electric Vehicle Washington State/create_table.sql
@@ -0,0 +1,20 @@
+CREATE TABLE ev_population (
+	"VIN (1-10)" char(10),
+	County text,
+	City text,
+	State char(2),
+	"Postal Code" char(5),
+	"Model Year" smallint,
+	Make text,
+	Model text,
+	"Electric Vehicle Type" text,
+	"Clean Alternative Fuel Vehicle (CAFV) Eligibility" text,
+	"Electric Range" int,
+	"Base MSRP" int,
+	"Legislative District" int,
+	"DOL Vehicle ID" text,
+	"Vehicle Location" text,
+	"Electric Utility" text,
+	"2020 Census Tract" text
+)
+
