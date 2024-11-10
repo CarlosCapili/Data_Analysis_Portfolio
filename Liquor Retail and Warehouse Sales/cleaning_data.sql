7b6e396e0124cecb3910100e71d9a7b5d2f05bd7
Carlos_Cap21 <59804756+CarlosCapili@users.noreply.github.com>
2024-11-10 17:51:05 -0500

    Add files via upload

---
+++ Liquor Retail and Warehouse Sales/cleaning_data.sql
@@ -0,0 +1,70 @@
+-- Create working copy of data
+CREATE TABLE liquor_sd_clean AS
+SELECT * FROM liquor_sales_data
+
+-- Update column names to convert to lowercase and remove of spaces in between words
+ALTER TABLE liquor_sd_clean
+	RENAME COLUMN "ITEM CODE" TO item_code
+
+ALTER TABLE liquor_sd_clean
+	RENAME COLUMN "ITEM DESCRIPTION" TO item_desc
+
+ALTER TABLE liquor_sd_clean
+	RENAME COLUMN "ITEM TYPE" TO item_type
+
+ALTER TABLE liquor_sd_clean
+	RENAME COLUMN "RETAIL SALES" TO retail_sales
+
+ALTER TABLE liquor_sd_clean
+	RENAME COLUMN "RETAIL TRANSFERS" TO retail_trans
+
+ALTER TABLE liquor_sd_clean
+	RENAME COLUMN "WAREHOUSE SALES" TO warehouse_sales
+
+Add a primary key 
+ALTER TABLE liquor_sd_clean
+	ADD COLUMN id SERIAL PRIMARY KEY 
+
+-- Remove duplicates
+WITH duplicates AS (
+	SELECT
+		id,
+		ROW_NUMBER() OVER(PARTITION BY "year", "month", supplier, item_code, item_desc, item_type ORDER BY item_code) AS row_num
+	FROM liquor_sd_clean
+)
+
+DELETE FROM liquor_sd_clean
+WHERE id IN (
+	SELECT id
+	FROM duplicates
+	WHERE row_num > 1
+)
+
+-- Check for NULL values in each column and handle appropriately 
+SELECT id, "year" FROM liquor_sd_clean WHERE "year" IS NULL
+SELECT id, "month" FROM liquor_sd_clean WHERE "month" IS NULL
+
+SELECT id, supplier FROM liquor_sd_clean WHERE supplier IS NULL
+DELETE FROM liquor_sd_clean WHERE supplier IS NULL
+
+SELECT id, item_code FROM liquor_sd_clean WHERE item_code IS NULL
+SELECT id, item_desc FROM liquor_sd_clean WHERE item_desc IS NULL
+
+SELECT id, item_type FROM liquor_sd_clean WHERE item_type IS NULL
+UPDATE liquor_sd_clean
+	SET item_type = 'WINE'
+	WHERE id = 96086
+
+SELECT id, retail_sales FROM liquor_sd_clean WHERE retail_sales IS NULL
+SELECT id, retail_trans FROM liquor_sd_clean WHERE retail_trans IS NULL
+SELECT id, warehouse_sales FROM liquor_sd_clean WHERE warehouse_sales IS NULL
+
+-- Add column to convert the month number column into month names (e.g. 1 for January, 2 for February)
+ALTER TABLE liquor_sd_clean
+	ADD COLUMN month_name text
+UPDATE liquor_sd_clean
+	SET month_name = TO_CHAR((DATE '2000-01-01' + (month - 1) * INTERVAL '1 month'), 'Month')
+
+SELECT * FROM liquor_sd_clean 
+
+	
---
+++ Liquor Retail and Warehouse Sales/create_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE liquor_sales_data (
+	YEAR int,
+	MONTH int,
+	SUPPLIER text,
+	"ITEM CODE" text,
+	"ITEM DESCRIPTION" text,
+	"ITEM TYPE" text,
+	"RETAIL SALES" numeric,
+	"RETAIL TRANSFERS" numeric,
+	"WAREHOUSE SALES" numeric
+)
